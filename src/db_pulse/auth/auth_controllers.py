8ebc762253190b6146dfccff5d5118738d4ed990
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2026-08-17 16:11:46 +0000

    login endpoint

--- src/db_pulse/auth/auth_controllers.py
+++ src/db_pulse/auth/auth_controllers.py
@@ -1,22 +1,54 @@
-from fastapi import APIRouter
+from datetime import timedelta
+
+from fastapi import APIRouter, HTTPException, status
 from pwdlib import PasswordHash
 
+from db_pulse.auth.login_request import LoginRequest
 from db_pulse.auth.register_request import RegisterRequest
+from db_pulse.auth.token import Token
 from db_pulse.auth.user_repository import UserRepository
+from db_pulse.auth.utils import ACCESS_TOKEN_EXPIRE_MINUTES, create_access_token
 
 router = APIRouter()
 
 user_repository = UserRepository()
 
+password_hash = PasswordHash.recommended()
+
 
 @router.post("/register")
 async def register(request: RegisterRequest):
-    password_hash = PasswordHash.recommended()
     hashed_password = password_hash.hash(request.password)
     request.password = hashed_password
 
     if user_repository.find_user_by_username(request.username):
-        return None
+        return
 
     user = request.to_user()
     return user_repository.add_user(user)
+
+
+@router.post("/login")
+async def login(request: LoginRequest):
+    user = user_repository.find_user_by_username(request.username)
+
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    if not password_hash.verify(request.password, user.password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+
+    return Token(access_token=access_token, token_type="bearer")
